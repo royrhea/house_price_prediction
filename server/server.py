7fc0b69d17611508e14c535dbb0dfe29f1fdfcc7
Rishita <royrishita744@gmail.com>
2026-04-28 01:47:28 +0530

    adding server and ui files

--- server/server.py
+++ server/server.py
@@ -1,10 +1,27 @@
 from flask import Flask,request,jsonify
+import util
 app=Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "Hi"
+@app.route('/get_location_names',methods=['GET'])
+def get_location_names():
+    response=jsonify({
+        'locations':util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+@app.route('/predict_home_price',methods=['GET','post'])
+def predict_home_price():
+    total_sqft=float(request.form['total_sqft'])
+    location=request.form['location']
+    bhk=int(request.form['bhk'])
+    bath=int(request.form['bath'])
+    response=jsonify({
+        'estimated_price': util.get_estimated_price(sqft=total_sqft, location=location,bhk=bhk,bath=bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__=="__main__":
     print("starting python flask server for home price prediction...")
-    app.run()
+    util.load_saved_artifacts()
+    app.run(debug=True)
